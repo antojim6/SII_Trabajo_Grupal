7a610c0a04f3a7ef26fad3f0df45a1b1fa8189fe
Francisco Perdiguero <fran_n_45@hotmail.com>
2020-04-01 12:05:44 +0200

    Creada entidad Actividad

---
+++ src/proyectos/Actividad.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectos;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author franciscoperdiguero
+ */
+@Entity
+public class Actividad implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private String conocimientos_necesarios;
+    private Date fecha_inicio;
+    private Date fecha_finalizacion;
+
+    public Long getId() {
+        return id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getConocimientos_necesarios() {
+        return conocimientos_necesarios;
+    }
+
+    public Date getFecha_inicio() {
+        return fecha_inicio;
+    }
+
+    public Date getFecha_finalizacion() {
+        return fecha_finalizacion;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setConocimientos_necesarios(String conocimientos_necesarios) {
+        this.conocimientos_necesarios = conocimientos_necesarios;
+    }
+
+    public void setFecha_inicio(Date fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
+    }
+
+    public void setFecha_finalizacion(Date fecha_finalizacion) {
+        this.fecha_finalizacion = fecha_finalizacion;
+    }
+    
+    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Actividad)) {
+            return false;
+        }
+        Actividad other = (Actividad) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "proyectos.Actividad[ id=" + id + " ]";
+    }
+    
+}
