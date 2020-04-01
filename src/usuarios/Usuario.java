8b136ebdca1ff566e0d6e3d5cd9c6f13a69a180a
antojim6 <48283387+antojim6@users.noreply.github.com>
2020-04-01 12:54:38 +0200

    Herencia.

--- src/usuarios/Usuario.java
+++ src/usuarios/Usuario.java
@@ -12,6 +12,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -22,6 +24,7 @@ import proyectos.Actividad;
  * @author Antonio Jiménez Godínez
  */
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
